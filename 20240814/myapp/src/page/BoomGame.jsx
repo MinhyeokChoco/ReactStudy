8caff3cae56b37e30107568734a955982abf661a
MinhyeokChoco <smile_forever@nate.com>
2024-08-16 15:40:38 +0900

    BoomGame.jsx modify

--- 20240814/myapp/src/page/BoomGame.jsx
+++ 20240814/myapp/src/page/BoomGame.jsx
@@ -1,13 +1,11 @@
-import { Component } from 'react';
-import Boom from '../components/Boom';
-import BoomImage from '../image/boom.png';
+import { Component } from "react";
+import Boom from "../components/Boom";
 
 export default class BoomGame extends Component {
-    // 상태 변수 초기화
     constructor() {
         super()
         this.state = {
-            over: false,
+            Over: false,
             Count: 0,
             boom: Math.floor(Math.random() * 9),
             boomCount: 9
@@ -15,13 +13,13 @@ export default class BoomGame extends Component {
     }
 
     componentDidMount() {
-        console.log(this.state)
         // 최초에 한번 호출
+        console.log(this.state)
     }
 
     componentDidUpdate() {
-        console.log('설마 게임 끝 ?')
-        // 상태 변수가 변경되면 호출 해야 할 로직
+        // 상태 변수가 변경되면 호출해야할 로직
+        console.log("설마 게임 끝?");
     }
 
     componentWillUnmount() {
@@ -29,42 +27,30 @@ export default class BoomGame extends Component {
     }
 
     setCount = () => {
-        this.setState({ ...this.state, count: this.state.count + 1 });
+        this.setState({ ...this.state, Count: this.state.Count + 1 });
     }
 
     gameOver = () => {
-        setTimeout(() => {
-            this.setState({ ...this.state, over: true });
-        }, 1000);
+        this.setState({ ...this.state, Over: true });
     }
 
     setBoom = () => {
+        // 폭탄 초기화
         const count = this.state.boomCount;
-        return (Array(count).fill(null).map(() => <Boom gameOver={this.gameOver}
-            setValue={index === this.state.boom ? this.seCount : this.setCount} over={this.state.over} />))
+        return (Array(count).fill(null).map((e, index) => <Boom gameOver={this.gameOver}
+            setValue={index === this.state.boom ? this.gameOver : this.setCount} Over={this.state.Over} />))
     }
 
     render() {
-        return (
-            <div className="boom-game-manager">
-                <span>{this.state.Count}</span>
-                {this.state.over ? <div>게임 오버</div> : <this.setBoom />}
-            </div>
-        )
+        return (<div className="boom-game-manger">
+            <span>{this.state.Count}</span>
+            {this.state.Over ? <div>게임 오버</div> : <this.setBoom />}
+        </div>)
     }
 }
 
-// { BoomGame }
-// BoomGame
 
-// 그러면 default로 하나만 export 내보냈는데
-// 객체의 키 값으로 받아올 수 있다고 하셨는데
-// import로 가져오고 클래스 안에 객체를 사용하고 싶을 때 구조분해할당해서 사용 하면 되는지 여쭤보기
-
-// img src 뒤 alt는 무엇인지 알아보기
-// img가 로드가 되지 않았을 때의 대체할 텍스트를 작성
-
-// 1 게임 매니저 ok
-// 2 폭탄 ok
-// 3 게임 매니저의 역할 분리(게임 오버, 게임 점수 ok, 랜덤한 지뢰의 값)
-// 4 폭탄의 역할(활성화인지, 비활성화인지 ok)
+// 1 게임 매니저 V
+// 2 폭탄 V
+// 3 게임 매니저의 역활 분리(게임 점수 V, 게임 오버 , 랜덤한 지뢰의 값 )
+// 4 폭탄의 역활(활성화인지 비활성화인지 V)
